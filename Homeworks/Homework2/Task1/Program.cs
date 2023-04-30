4cfce3b7f5ca270fb790b4c3a145a03411f95505
Viktoria Polyakovich <polyakovichvika@yandex.by>
2023-04-30 20:16:21 +0300

    Homework2

---
+++ Homeworks/Homework2/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int SecondDigit (int num)
+{
+    int secondnum = num/10%10;
+    return secondnum;
+}
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Второе число: -> {SecondDigit(num)}");
---
+++ Homeworks/Homework2/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool ValidateNumber(int number) {
+if (number == 6 || number ==7 ) {
+    Console.WriteLine($"{number}->да");
+    return true;
+}
+Console.WriteLine($"{number}-> нет");
+return false;
+}
+
+int number = Prompt("Введите число: ");
+ValidateNumber(number);
