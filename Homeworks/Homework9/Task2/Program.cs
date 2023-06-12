e87bb353f220a45d6752158a327a447e41de5fe5
Viktoria Polyakovich <polyakovichvika@yandex.by>
2023-06-12 10:52:42 +0300

    Homework9

---
+++ Homeworks/Homework9/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+
+int ShowNumbers(int m, int n)
+{
+    if (n > m)
+    {
+        return m + ShowNumbers(m+1, n);
+    }
+    else return m;
+}
+
+Console.WriteLine(ShowNumbers(4, 8));
---
+++ Homeworks/Homework9/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+
+int Akkermanfun(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkermanfun(m - 1, 1);
+    }
+    else
+    {
+        return (Akkermanfun(m - 1, Akkermanfun(m, n - 1)));
+    }
+}
+
+
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Akkermanfun(m, n)); 
